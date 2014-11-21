36229bc30ae7cfb8768a9a365b8bd537a30bf6f8
Saleh <saleh@cise.ufl.edu>
2014-11-21 15:40:13 -0500

    Almost working without using OBJ files. only problem is translation.

--- export2sofa.py
+++ export2sofa.py
@@ -27,30 +27,32 @@ def createMechanicalObject(o):
     o.rotation_mode = "ZYX"
     t = ET.Element("MechanicalObject",template="Vec3d",name="MO")
     t.set("translation", vector_to_string(o.location))
-    t.set("rotation", vector_to_string(o.rotation_euler))
+    #t.set("rotation", vector_to_string(o.rotation_euler))
     t.set("scale", vector_to_string(o.scale))
     return t
     
 def exportSoftBody(o):
-    meshFN = o.name + ".obj"
     t = ET.Element("Node",name=o.name)
     t.append(ET.Element("EulerImplicitSolver"))
     t.append(ET.Element("CGLinearSolver",template="GraphScattered"))
     t.append(createMechanicalObject(o))
     t.append(ET.Element("UniformMass",template="Vec3d"))
+    v = ET.Element("Node",name="Visual")
+    og = exportVisual(o, name = 'Visual', with_transform = False)
+    og.set('template', 'ExtVec3f')
+    v.append(og)
+    v.append(ET.Element("BarycentricMapping",template="Vec3d,ExtVec3f",object1="../MO",object2="Visual"))
+    t.append(v)
+
     # set n later
-    t.append(ET.Element("SparseGridTopology",fileTopology=meshFN,n="10 10 10"))
+    t.append(ET.Element("SparseGridTopology",position="@Visual/Visual.position",quads="@Visual/Visual.quads",triangles="@Visual/Visual.triangles",n="10 10 10"))
     # set young modulus later
     t.append(ET.Element("HexahedronFEMForceField",template="Vec3d"))
 
-    v = ET.Element("Node",name="Visual")
-    v.append(ET.Element("OglModel",template="ExtVec3f",name="Visual",filename=meshFN))
-    v.append(ET.Element("BarycentricMapping",template="Vec3d,ExtVec3f",object2="Visual"))
-    t.append(v)
     
     c = ET.Element("Node",name="Collision")    
-    c.append(ET.Element("MeshTopology",fileTopology=meshFN))
-    c.append(ET.Element("MechanicalObject",template="Vec3d"))
+    c.append(ET.Element("MeshTopology",position="@../Visual/Visual.position",quads="@../Visual/Visual.quads",triangles="@../Visual/Visual.triangles",translation=vector_to_string(o.location)))
+    c.append(ET.Element("MechanicalObject",template="Vec3d",name="MOC"))
     c.extend([ ET.Element("PointModel"), ET.Element("LineModel"), ET.Element("TriangleModel") ])
     c.append(ET.Element("BarycentricMapping",template="Vec3d,Vec3d",input="@../",output="@./"))
     t.append(c)
@@ -58,24 +60,63 @@ def exportSoftBody(o):
     return t
   
 def exportObstacle(o):
-    meshFN = o.name + ".obj"
     t = ET.Element("Node",name=o.name)
-    t.append(ET.Element("OglModel",name="Visual",filename=meshFN))
-    t.append(ET.Element("MeshTopology",fileTopology=meshFN))
+    t.append(exportVisual(o, name = 'Visual', with_transform = False))
+    t.append(ET.Element("MeshTopology",position="@Visual.position",quads="@Visual.quads",triangles="@Visual.triangles"))
     t.append(createMechanicalObject(o))
     t.extend([ ET.Element("PointModel",moving='0',simulated='0')
         , ET.Element("LineModel",moving='0',simulated='0')
         , ET.Element("TriangleModel",moving='0',simulated='0') ])
     t.append(ET.Element("IdentityMapping",object1="MO",object2="Visual"))
     return t
-        
-def exportVisual(o):
-    meshFN = o.name + ".obj"
+    
+from array import array
+    
+def exportVisual(o, name = None,with_transform = True):
+    m = o.data
     o.rotation_mode = "ZYX"
-    t = ET.Element("OglModel",filename=meshFN,name=o.name)
-    t.set("translation", vector_to_string(o.location))
-    t.set("rotation", vector_to_string(o.rotation_euler))
-    t.set("scale", vector_to_string(o.scale))
+    
+    t = ET.Element("OglModel",name=name or o.name)
+    
+    if with_transform :
+        t.set("translation", vector_to_string(o.location))
+        t.set("rotation", vector_to_string(o.rotation_euler))
+        t.set("scale", vector_to_string(o.scale))
+
+    position = [ vector_to_string(v.co) for v in m.vertices]
+    t.set("position", ' '.join(position))
+    normal   = [ vector_to_string(v.normal) for v in m.vertices]
+    t.set("normal", ' '.join(normal))
+
+    triangles = [ vector_to_string(f.vertices) for f in m.polygons if len(f.vertices) == 3 ]
+    quads     = [ vector_to_string(f.vertices) for f in m.polygons if len(f.vertices) == 4 ]
+    t.set("triangles", ' '.join(triangles))
+    t.set("quads", ' '.join(quads))    
+
+    if len(m.uv_layers) >= 1 :
+        uvl = m.uv_layers[0].data
+        ## allocate a mapping between vertex indices and loop indices
+        mapping = array('I',[ 0 for i in range(0,len(m.vertices)) ])
+        for l in m.loops: mapping[l.vertex_index] = l.index     
+        texcoords = [ vector_to_string(uvl[mapping[i]].uv) for i in range(0,len(m.vertices))]
+        t.set("texcoords", ' '.join(texcoords))
+
+    if len(m.materials) >= 1 :
+        mat = m.materials[0]
+        
+        d = vector_to_string(mat.diffuse_color*mat.diffuse_intensity) 
+        a = vector_to_string(mat.diffuse_color*mat.ambient) 
+        s = vector_to_string(mat.specular_color*mat.specular_intensity)  
+        e = vector_to_string(mat.diffuse_color*mat.emit) 
+        ss = mat.specular_hardness
+        text = "Default Diffuse 1 %s 1 Ambient 1 %s 1 Specular 1 %s 1 Emissive 1 %s 1 Shininess 1 %d " % (d,a,s,e,ss)
+        
+        t.set("material", text)
+        if len(mat.texture_slots) >= 1 and mat.texture_slots[0] != None :
+            tex = mat.texture_slots[0].texture
+            if tex.type == 'IMAGE' :
+                t.set("texturename", bpy.path.abspath(tex.image.filepath))
+                t.set("material","")
     return t
     
 
@@ -90,21 +131,19 @@ def exportScene(scene,dir):
     root= ET.Element("Node")
     root.set("name", "root")
     root.set("gravity",vector_to_string(scene.gravity))
+    
+    if scene.get('displayFlags') != None :
+        root.append(ET.Element("VisualStyle",displayFlags=scene['displayFlags']))
     # for late alarmDistance="0.1"  contactDistance="0.005"  attractDistance="0.01"
     root.append(ET.Element("DefaultPipeline"))
     root.append(ET.Element("BruteForceDetection"))
     root.append(ET.Element("MinProximityIntersection",useSurfaceNormals="1"))
     root.append(ET.Element("DefaultContactManager"))
     
-    root.append(ET.Element("LightManager"))
+    #root.append(ET.Element("LightManager"))
     root.append(ET.Element("OglSceneFrame"))
     for o in scene.objects: 
         if o.type == "MESH":
-            meshFN = o.name + ".obj"
-            o.select = True
-            bpy.ops.export_scene.obj(filepath = os.path.join(dir, meshFN), use_selection=True, axis_forward="Y", axis_up="Z")
-            o.select = False
-            
             if has_modifier(o,'SOFT_BODY'):
                 t = exportSoftBody(o)
             elif has_modifier(o,'COLLISION'):
@@ -207,7 +246,7 @@ def register():
     wm = bpy.context.window_manager
     km = wm.keyconfigs.addon.keymaps.new(name='Object Mode', space_type='EMPTY')
 
-    kmi = km.keymap_items.new(RunSofaOperator.bl_idname, 'F5', 'PRESS', ctrl=True)
+    kmi = km.keymap_items.new(RunSofaOperator.bl_idname, 'F5', 'PRESS')
     addon_keymaps.append((km, kmi))
     #kmi.properties.total = 4
 
@@ -227,6 +266,9 @@ def unregister():
 if __name__ == "__main__":
     register()
     #bpy.ops.export.tosofa('INVOKE_DEFAULT')
+    bpy.ops.scene.runsofa('INVOKE_DEFAULT')
+
+
 
 
 
