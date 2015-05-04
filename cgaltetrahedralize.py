7a63713a18797d5195a869a08452ba13f4327215
Saleh <saleh@cise.ufl.edu>
2015-05-04 15:11:02 -0400

    Replace TetGen with CGALTetrahedralize, still needs parameters fixed

---
+++ cgaltetrahedralize.py
@@ -0,0 +1,119 @@
+from ctypes import *
+import numpy as np
+import numpy.ctypeslib as npc 
+
+
+
+p_int = POINTER(c_int)
+c_real = c_double
+p_real = POINTER(c_real)
+c_void = None
+
+class TriangleMesh(Structure):
+    _fields_ = [
+        ('vertex', p_real),
+        ('vertexCount', c_int),
+        ('triangle', p_int),
+        ('triangleCount', c_int)
+    ]
+
+    def __init__(self):
+        self.vertex = None
+        self.triangle = None
+        self.vertexCount = 0
+        self.triangleCount = 0
+
+    def set_vertices(self, v):
+        if isinstance(v, np.ndarray) and v.shape[1] == 3:
+            self.vertex = npc.as_ctypes(v.flatten())
+            self.vertexCount = len(v)
+        else:
+            raise ArgumentError("numpy array expected")
+
+    def get_vertices(self):
+        return npc.as_array(self.vertex, shape=(self.vertexCount,3))
+
+    def set_triangles(self, t):
+        if isinstance(t, np.ndarray) and t.shape[1] == 3:
+            self.triangle = npc.as_ctypes(t.flatten())
+            self.triangleCount = len(t)
+        else:
+            raise ArgumentError("numpy array expected")
+
+    def get_triangles(self):
+        return npc.as_array(self.triangle, shape=(self.triangleCount,3))
+
+    vertices = property(get_vertices, set_vertices)
+    triangles = property(get_triangles, set_triangles)
+
+class TetrahedralMesh(Structure):
+    _fields_ = [
+        ('vertex', p_real),
+        ('vertexCount', c_int),
+        ('triangle', p_int),
+        ('triangleCount', c_int),
+        ('tetrahedron', p_int),
+        ('tetrahedronCount', c_int)
+    ]
+
+    def __init__(self):
+        self.vertex = None
+        self.triangle = None
+        self.tetrahedron = None
+        self.vertexCount = 0
+        self.triangleCount = 0
+        self.tetrahedronCount = 0
+
+    def set_vertices(self, v):
+        if isinstance(v, np.ndarray) and v.shape[1] == 3:
+            self.vertex = npc.as_ctypes(v.flatten())
+            self.vertexCount = len(v)
+        else:
+            raise ArgumentError("numpy array expected")
+
+    def get_vertices(self):
+        return npc.as_array(self.vertex, shape=(self.vertexCount,3))
+
+    def set_triangles(self, t):
+        if isinstance(t, np.ndarray) and t.shape[1] == 3:
+            self.triangle = npc.as_ctypes(t.flatten())
+            self.triangleCount = len(t)
+        else:
+            raise ArgumentError("numpy array expected")
+
+    def get_triangles(self):
+        return npc.as_array(self.triangle, shape=(self.triangleCount,3))
+
+    def set_tetrahedra(self, t):
+        if isinstance(t, np.ndarray) and t.shape[1] == 4:
+            self.tetrahedron = npc.as_ctypes(t.flatten())
+            self.tetrahedronCount = len(t)
+        else:
+            raise ArgumentError("numpy array expected")
+
+    def get_tetrahedra(self):
+        return npc.as_array(self.tetrahedron, shape=(self.tetrahedronCount,4))
+
+
+    vertices = property(get_vertices, set_vertices)
+    triangles = property(get_triangles, set_triangles)
+    tetrahedra = property(get_tetrahedra,set_tetrahedra)
+
+
+
+
+from os import path, environ
+from sys import platform
+if platform == 'win32':
+    library_file = 'cgaltetrahedralize.dll'
+elif platform == 'unix':
+    library_file = 'libcgaltetrahedralize.so'
+else:
+    raise RuntimeError("Platform not supported");
+
+environ['PATH'] = environ['PATH'] + ';' + path.join(path.dirname(__file__), 'cgaltetrahedralize');
+libcgaltetrahedralize = cdll.LoadLibrary(library_file)
+
+tetrahedralize = libcgaltetrahedralize.tetrahedralize
+tetrahedralize.argtypes = [ POINTER(TriangleMesh), POINTER(TetrahedralMesh) ]
+
--- mesh2tetra.py
+++ mesh2tetra.py
@@ -1,23 +1,26 @@
-from .tetgen import *
+from .cgaltetrahedralize import *
 from bpy import context as C
 import numpy as N
 
 def convert(o, scn):
-    a = TetGenIO()
-    a.firstnumber = 0
-    a.mesh_dim = 3
+
+
     m = o.to_mesh(scn, True, 'PREVIEW')
-    position =  N.empty((len(m.vertices),3))
+    position =  N.empty((len(m.vertices),3),order='C')
     for i, v in enumerate(m.vertices):
         position[i][0] = v.co[0]
         position[i][1] = v.co[1]
         position[i][2] = v.co[2]
-    a.points = position
-    a.facets = [ Facet([ Polygon(f.vertices) ]) for f in m.polygons ]
-    
-    b = TetGenIO()
-    # Tetrahedralize the PLC. Switches are chosen to read a PLC (p),
-    #   do quality mesh generation (q) with a specified quality bound
-    #   (1.414), and apply a maximum volume constraint (a0.1).
-    tetrahedralize(b"p", pointer(a), pointer(b), None, None)
-    return ( b.points, b.tetrahedra )
+    triangles = N.empty((len(m.polygons),3),dtype=int,order='C')
+    for i, f in enumerate(m.polygons):
+        triangles[i][0] = f.vertices[0]
+        triangles[i][1] = f.vertices[1]
+        triangles[i][2] = f.vertices[2]
+
+    inmesh = TriangleMesh()
+    inmesh.vertices = position
+    inmesh.triangles = triangles
+
+    outmesh = TetrahedralMesh()
+    tetrahedralize(pointer(inmesh),pointer(outmesh))
+    return ( outmesh.vertices, outmesh.tetrahedra )
