71743a84773b217908c773d2629d1dd06ee2952e
Saleh Dindar <saleh@cise.ufl.edu>
2015-05-20 16:58:06 -0400

    Adding a script to create a distribution

---
+++ mkzip.sh
@@ -0,0 +1,11 @@
+zip -r blender2sofa-$(hg id -i).zip blender2sofa.py \
+	export2sofa/cgaltetrahedralize.py \
+	export2sofa/libmpfr-4.dll\
+	export2sofa/tetgen.py \
+	export2sofa/ui.py\
+	export2sofa/mesh2tetra_cgal.py\
+	export2sofa/mesh2tetra_tetgen.py \
+	export2sofa/tetgen.dll\
+	export2sofa/CGAL-vc120-mt-4.6.dll \
+	export2sofa/CGALTetrahedralize.dll \
+	export2sofa/libgmp-10.dll
